a66495727fb8289eac400e0efc45d3d07e989931
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-12-14 12:09:22 -0300

    feat: create primary_button  component on folder path common/widgets

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,5 +1,6 @@
 import 'package:finance_app/common/constants/app_colors.dart';
 import 'package:finance_app/common/constants/app_text_styles.dart';
+import 'package:finance_app/common/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -53,44 +54,6 @@ class OnboardingPage extends StatelessWidget {
   }
 }
 
-class PrimaryButton extends StatelessWidget {
-  final VoidCallback? onPressed;
-  final String text;
 
-  final BorderRadius _borderRadius = const BorderRadius.all(Radius.circular(38.0));
-
-  const PrimaryButton({
-    Key? key,
-    this.onPressed,
-    required this.text,
-  }): super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Ink(
-        height: 54.0,
-        decoration: BoxDecoration(
-          borderRadius: _borderRadius,
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: onPressed != null ? AppColors.primaryGradient : AppColors.secondGradient
-          ),
-        ),
-        child: InkWell(
-          borderRadius: _borderRadius,
-          onTap: onPressed,
-          child: Align(
-            child: Text(
-              text,
-              style: AppTextStyles.mediumText18.copyWith(color: AppColors.white),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
         
